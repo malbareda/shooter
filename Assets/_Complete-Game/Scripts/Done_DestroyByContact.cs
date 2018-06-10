6e3cd863fe9bb52d4b9947e722e055e6f56b8171
malbareda <dm.albareda@gmail.com>
2018-06-10 03:47:14 +0200

    Initial commit

---
+++ Assets/_Complete-Game/Scripts/Done_DestroyByContact.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+    public bool divisible;
+    public GameObject smaller;
+	private Done_GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <Done_GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Boundary" || other.tag == "Enemy")
+		{
+			return;
+		}
+
+        if (divisible)
+        {
+            print("ok");
+            Vector3 spawnPosition = new Vector3(transform.position.x-1, transform.position.y, transform.position.z-1);
+            Instantiate(smaller, spawnPosition, Quaternion.identity);
+             spawnPosition = new Vector3(transform.position.x+1, transform.position.y, transform.position.z-1);
+            Instantiate(smaller, spawnPosition, Quaternion.identity);
+             spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z+1);
+            Instantiate(smaller, spawnPosition, Quaternion.identity);
+            Destroy(other.gameObject);
+        }
+
+		if (explosion != null)
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+            
+        }
+
+		if (other.tag == "Player")
+		{
+            
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.death();
+            Destroy(gameObject);
+            if (gameController.gameOver)
+            {
+                Destroy(other.gameObject);
+            }
+            return;
+        }
+		
+		gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Complete-Game/Scripts/Done_TriWeaponController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_TriWeaponController : MonoBehaviour
+{
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+
+    void Start()
+    {
+        InvokeRepeating("Fire", delay, fireRate);
+    }
+
+    void Fire()
+    {
+        Vector3 newshot = new Vector3(shotSpawn.position.x, shotSpawn.position.y, shotSpawn.position.z);
+        
+        Instantiate(shot, newshot, shotSpawn.rotation);
+        newshot.x--;
+        shotSpawn.Rotate(0, -30, 0);
+        Instantiate(shot, newshot, shotSpawn.rotation);
+        newshot.x += 2;
+        shotSpawn.Rotate(0, 60, 0);
+        Instantiate(shot, newshot, shotSpawn.rotation);
+        shotSpawn.Rotate(0, -30, 0);
+        GetComponent<AudioSource>().Play();
+    }
+}
